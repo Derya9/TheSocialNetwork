ddd0ff31ddf34ec85701245d0a5984defb06946b
derya <d.k.gesierich@gmail.com>
2024-04-17 10:58:46 +0200

    added persons array to constants.php

--- data/constants.php
+++ data/constants.php
@@ -1,1 +1,11 @@
 <?php
+
+require_once ("person.php");
+
+$persons = [
+    new Person("Malek", "Al-Nakechbandi",31),
+    new Person("Derya", "Gesierich", 32),
+    new Person("Marc", "Hartleib", 32),
+    new Person("Emad", "Easa", 29),
+    new Person("Guillermo", "Herrero", 28)
+]
